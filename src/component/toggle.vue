b4853b500c748defb5fa9a2d66c9700ca727651d
Allenyooo <smpss901@gmail.com>
2024-05-26 11:42:39 +0800

    add reset.css

---
+++ src/component/toggle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <button @click="toggleClass">Toggle Class</button>
+      <p :class="{ show: isShow }">點擊btn會切換p的class</p>
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        isShow: false
+      };
+    },
+    methods: {
+      toggleClass() {
+        this.isShow = !this.isShow;
+      }
+    }
+  };
+</script>
+  
+<style scoped>
+
+</style>
+  
