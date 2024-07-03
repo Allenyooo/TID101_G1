62f9eb939c3d07fee5821f3794282cb7122449b6
roy234578 <t23284g3@gmail.com>
2024-07-03 14:19:40 +0800

    後台 刪除修正

---
+++ public/php/Bk/BkDiscount/discountDelete.php
@@ -0,0 +1,28 @@
+<?php
+      include("../../conn.php");
+
+  
+        // $ID = 6;
+    
+        $Bkdelete = json_decode(file_get_contents("php://input"), true);
+
+        // $Input = $Bksearh['Input'];
+        // $BkInput = '%'.$Input.'%';
+
+        // $Start = $Bksearh['Start'];
+        // $End = $Bksearh['End'];
+
+        $BkdeleteID = $Bkdelete['deleteId'];
+       
+
+
+        $sql = "DELETE from VOUCHER
+	                   where ID = ?";
+
+            $statement = $pdo->prepare($sql);
+            $statement->bindValue(1 , $BkdeleteID);
+
+            $statement->execute();
+
+    
+?>
